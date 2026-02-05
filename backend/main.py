285b0c5820771b488c622516af73284a5b7bb115
Shalaka Bapat <shalaka@Shalakas-MBP.lan>
2026-02-05 18:52:04 -0500

    Fix CORS to allow Vercel

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="API", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
